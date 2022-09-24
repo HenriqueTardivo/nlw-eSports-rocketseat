ad5ade3d74b1d8470a4021ac5e555f43a10a58c2
Henrique Tardivo <henrique.tardivo@gmail.com>
2022-09-24 12:35:03 -0300

    mobile

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Image, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GameParams } from "../../@types/navigation";
 import { Background } from "../../components/Background";
@@ -8,15 +8,24 @@ import { styles } from "./styles";
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { THEME } from "../../theme";
 import { Heading } from "../../components/Heading";
+import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { useEffect, useState } from "react";
 
 export function Game() {
   const route = useRoute();
   const navigator = useNavigation();
   const game = route.params as GameParams;
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
 
   function handleGoBack() {
     navigator.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  });
 
   return (
     <Background>
@@ -41,6 +50,25 @@ export function Game() {
         />
 
         <Heading title={game.title} subtitle="Conecte-se e comece a jogar" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          contentContainerStyle={
+            duos.length === 0 ? styles.emptyListContent : styles.contentList
+          }
+          style={styles.containerList}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anuncios publicados ainda
+            </Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   );
